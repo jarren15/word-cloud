14d34161486ba778e8ce8dc4a6519a64dbe0b125
John Arren Novilla <John.Arren.Novilla@ibm.com>
2022-02-08 01:34:59 +0800

    Update styling

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
-function submitResponse(){
+function submitResponse(e){
+    e.preventDefault();
     var learnerResponse = document.querySelector('[data-wordCloud-input]').value;
 
-    db.collection("responses").add({
-        word: learnerResponse,
-    }).then(data => console.log(`Successfully added ${data}`));
+    if(learnerResponse != ''){
+        db.collection("responses").add({
+            word: learnerResponse,
+        }).then(data => console.log(`Successfully added ${data}`));
+    }
 
     document.querySelector('[data-wordCloud-input]').value = '';
 }
@@ -24,10 +27,11 @@ function submitResponse(){
 db.collection("responses").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         var fontSize = Math.floor(Math.random() * 5) + 1;
+        var fontWeight = Math.floor(Math.random() * 900) + 100;
         var {word} = doc.data();
         var span = document.createElement('SPAN');
         span.innerHTML = word;
-        span.style.cssText = `font-size: ${fontSize}rem;`;
+        span.style.cssText = `font-size: ${fontSize}rem; font-weight: ${fontWeight};`;
         document.querySelector('[data-wordCloud-container]').appendChild(span);
     });
 });
@@ -36,10 +40,11 @@ db.collection("responses").onSnapshot((querySnapshot) => {
     document.querySelector('[data-wordCloud-container]').innerHTML = '';
     querySnapshot.forEach((doc) => {
         var fontSize = Math.floor(Math.random() * 5) + 1;
+        var fontWeight = Math.floor(Math.random() * 900) + 100;
         var {word} = doc.data();
         var span = document.createElement('SPAN');
         span.innerHTML = word;
-        span.style.cssText = `font-size: ${fontSize}rem;`;
+        span.style.cssText = `font-size: ${fontSize}rem; font-weight: ${fontWeight};`;
         document.querySelector('[data-wordCloud-container]').appendChild(span);
     });
 });
